ba40f6ff59911f74a6794db31f1ed10c75a433c6
zml <747765207@qq.com>
2022-05-20 14:48:09 +0800

    mdf '更改样式 警告屏蔽'

--- src/plugin/highlight.js
+++ src/plugin/highlight.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Hljs from 'highlight.js'
 import 'highlight.js/styles/googlecode.css'
+Hljs.configure({
+  ignoreUnescapedHTML: true
+})
 let Highlight = {}
 Highlight.install = ()=> {
   Vue.directive('highlight', function (el) {
     let blocks = el.querySelectorAll('pre code');
     blocks.forEach((block) => {
-      Hljs.highlightBlock(block)
+      Hljs.highlightElement(block)
     })
   })
 }
